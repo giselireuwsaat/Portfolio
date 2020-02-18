906d475133e77fa9fec822fa7334da01dbd353b1
giselireuwsaat <givasconcelos1@gmail.com>
2020-02-18 15:37:02 -0300

    chore:finished all page

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let nav = document.querySelector('#nav');
+let sort = document.querySelector('#sorte');
+
+
+window.onscroll = function () {
+    if (window.pageYOffset > 60) {
+        sort.style.background = 'rgb(216,216,216)'
+        nav.style.background = 'rgb(216,216,216)'
+        nav.style.height = '3em'
+        nav.style.boxShadow = "0px 3px 4px 0px rgba(50, 50, 50, 0.38)"
+
+    } else {
+        nav.style.background = 'transparent';
+        sort.style.background = 'transparent';
+        nav.style.boxShadow = "none"
+    }
+}
