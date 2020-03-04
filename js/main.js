1abdb885919aa1c8d5c8eddcdfd4f10ed929fd18
giselireuwsaat <givasconcelos1@gmail.com>
2020-03-04 17:44:23 -0300

    fix: toggle botton

--- js/main.js
+++ js/main.js
@@ -3,15 +3,16 @@ let sort = document.querySelector('#sorte');
 
 
 window.onscroll = function () {
-    if (window.pageYOffset > 10) {
-        sort.style.background = 'rgb(216,216,216)'
+    if (window.pageYOffset > 50) {
+        // sort.style.background = 'rgb(216,216,216)'
         nav.style.background = 'rgb(216,216,216)'
         nav.style.height = '3em'
         nav.style.boxShadow = "0px 3px 4px 0px rgba(50, 50, 50, 0.38)"
 
     } else {
         nav.style.background = 'transparent';
-        sort.style.background = 'transparent';
+        // sort.style.background = 'transparent';
         nav.style.boxShadow = "none"
     }
 }
+
